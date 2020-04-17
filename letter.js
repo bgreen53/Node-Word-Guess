31cf13a97bd682ba925de117e1b9be9a33aa7e51
bgreen53 <green.barry53@gmail.com>
2020-04-16 21:40:10 -0700

    restructured letter

--- letter.js
+++ letter.js
@@ -1,66 +1,29 @@
-var inquirer = require('inquirer');
-var Letter = function(){
 
-this.game = function(){
-  inquirer
-  .prompt([
-    {type: "confirm",
-      message: "Would you like to play a game?",
-      name: "confirm"},
-
-  ])
-  .then(function(inquirerResponse) {
-    
-      if(inquirerResponse.confirm) { 
-        addBlanks()
-      }else{
-        console.log("Maybe another time")
+var Letter = function (alpha) {
+  this.alpha = alpha
+  this.word = ""
+  this.guessed = false
+  this.display = function () {
+    for (var i = 0; i < this.word.length; i++) {
+      if (this.alpha === " ") {
+        return " "
+      } else if (this.guessed) {
+        return this.alpha
+      } else {
+        return "_"
       }
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
+
     }
-  });
+  };
+  this.checkLet= function(userGuess){
+    if(userGuess===this.alpha){
+      this.guessed=true
+    }
+  }
 }
-}
 
 
-function addBlanks (){
-  var word = "";
-    var blanks = [];
-  inquirer
-            .prompt([
-                 
-                    {
-                     type: "password",
-                     message: "Enter a word for your partner to guess",
-                      name: "word"
-                    },
-              ]).then(function(inquirerResponse) {
-                word= inquirerResponse.word.split("")
-               
-                for (var i=0; i < word.length; i++){
-                    if(word[i] === " "){
-                       blanks.push(" ")
-                   } else {
-                   blanks.push("_")
-                   }
-               
-                   
-                   };
-                   console.log("The word to guess is:")
-                   console.log(blanks.join(" "))
-                    
-              })
-      }
-      
-    
-var taco = new Letter
 
-taco.game()
 
 
 
